6d18010997031e93cd17f0a08d60042d2ddcdbd8
Sebastian Lavender <sebastianlavender@outlook.com>
2019-02-05 16:42:54 +0000

    Set minimum temperature of thermostat

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -25,6 +25,11 @@ describe("thermostat", function() {
       thermostat.down(5);
       expect(thermostat.temp).toEqual(15);
     });
+
+    it('does not go lower than 10 degrees', function() {
+      thermostat.down(22);
+      expect(thermostat.temp).toEqual(10);
+    });
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,16 @@
 function Thermostat () {
-  this.temp = 20
-}
+  this.temp = 20;
+  this._minTemp = 10;
+};
 
 Thermostat.prototype.up = function(number) {
   this.temp += number;
 };
 
 Thermostat.prototype.down = function(number) {
-  this.temp -= number;
+  if (this.temp - number < this._minTemp) {
+    this.temp = this._minTemp;
+  } else {
+    this.temp -= number;
+  };
 };
